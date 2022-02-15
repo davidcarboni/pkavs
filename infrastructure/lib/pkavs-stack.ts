f52c7501c5dc4800a3093131a8e898e85cfb109d
David Carboni <david@carboni.io>
2022-02-15 19:35:44 +0000

    Import fix

--- infrastructure/lib/pkavs-stack.ts
+++ infrastructure/lib/pkavs-stack.ts
@@ -1,4 +1,4 @@
-import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
+import { CfnOutput, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ecr from 'aws-cdk-lib/aws-ecr';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
